1fbb219c566c1ca28d65e3de24cbcbba93f0e21a
Michelle Antunes <117948294+MichelleAntunes@users.noreply.github.com>
2024-02-22 20:23:34 +0100

    added new react icon for x to close modals

--- src/pages/login/LoginPage.js
+++ src/pages/login/LoginPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { emailPattern, passwordPattern } from "../../constants/patterns";
+import { AiOutlineClose } from "react-icons/ai";
 
 const LoginPage = ({ onClose }) => {
   const login = (e) => {
@@ -15,9 +16,9 @@ const LoginPage = ({ onClose }) => {
           </h1>
           <button
             onClick={onClose}
-            className="text-gray-500 hover:text-gray-700 focus:outline-none"
+            className=" text-2xl text-gray-500 hover:text-gray-700 focus:outline-none"
           >
-            X
+            <AiOutlineClose />
           </button>
         </div>
         <form onSubmit={login} className="flex flex-col space-y-4">
--- src/pages/signup/SignupPage.js
+++ src/pages/signup/SignupPage.js
@@ -4,6 +4,7 @@ import {
   namePattern,
   passwordPattern,
 } from "../../constants/patterns";
+import { AiOutlineClose } from "react-icons/ai";
 
 const SignupPage = ({ onClose }) => {
   const signup = (e) => {
@@ -18,9 +19,9 @@ const SignupPage = ({ onClose }) => {
           </h1>
           <button
             onClick={onClose}
-            className="text-gray-500 hover:text-gray-700 focus:outline-none"
+            className=" text-2xl text-gray-500 hover:text-gray-700 focus:outline-none"
           >
-            X
+            <AiOutlineClose />
           </button>
         </div>
         <form onSubmit={signup} className="flex flex-col space-y-4">
