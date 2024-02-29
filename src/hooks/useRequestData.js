a87c6580ba680f05ef6e52d0f5e1f90cc29961d4
Michelle Antunes <117948294+MichelleAntunes@users.noreply.github.com>
2024-02-29 21:11:11 +0100

    Accessing the student list.

---
+++ src/hooks/useRequestData.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { getHeaders } from "../utils/storageManager";
+
+export function useRequestData(url, initialState) {
+  const [data, setData] = useState(initialState);
+
+  useEffect(() => {
+    axios
+      .get(url, getHeaders())
+      .then((response) => {
+        setData(response.data);
+        console.log(response);
+      })
+      .catch((error) => console.log(error.message));
+  }, [url]);
+
+  return data;
+}
